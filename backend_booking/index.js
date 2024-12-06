22cef581a432c22ee6dd563682689bfa62662574
Gowtham Raj <gmanda@gitam.in>
2024-12-06 19:26:55 +0530

    second

--- backend_booking/index.js
+++ backend_booking/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const port = 3001;
+app.use(cors({origin : "*"}));
 
 app.get("/" , (req , res) => {
     res.send("Hello World");
 });
+
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`);
+})
